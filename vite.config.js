77dc1cfd5cd91d8a62cd9072d5b31d445ebd2d70
gronius <vlivonika@gmail.com>
2024-08-08 13:16:33 +0200

    base url

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  base: 'https://gronius.github.io/convertor-temperatur-vue/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
